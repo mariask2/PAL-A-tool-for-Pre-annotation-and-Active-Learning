b7355a77820c8782c4fdd07d4aaf82392be08441
Maria Skeppstedt <mariask@dsv.su.se>
2018-06-06 21:58:16 +0200

    Made a hash of all the words in the set of predicted words

--- process_monitoring.py
+++ process_monitoring.py
@@ -5,6 +5,9 @@ import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 VECTORIZER_NAME = "vectorizer"
+PREDICTION = "PREDICTION"
+SCORE = "SCORE"
+MINORITY_CLASSES = "MINORITY_CLASSES"
 
 
 def get_full_process_monitoring_dir_path(path_slash_format, properties):
@@ -30,14 +33,38 @@ def init_process_monitoring(path_slash_format, properties, unlabelled_text_vecto
         else:
             return False
 
+
+def remove_underscore(word):
+    if len(word) == 3 and word[1] == "_":
+        return word[0]
+    else:
+        return word
+
+
 def write_process_monitoring_info(sentences_unlabelled, all_diffs, selected_indeces, ys, majority_class, inv_labelled_dict):
+    word_hash = {}
     for index, diffs, y in zip(selected_indeces, all_diffs, ys):
-        #print(index)
-        print(sentences_unlabelled[index])
-        print(diffs)
-        print(y)
-        print(majority_class)
-        print(inv_labelled_dict)
-        print("****")
+        current_sentence = sentences_unlabelled[index]
+        for word, conf, prediction in zip(current_sentence, diffs, y):
+            word = remove_underscore(word)
+            if word not in word_hash:
+                word_hash[word] = {PREDICTION:[], SCORE:[]}
+            word_hash[word][PREDICTION].append(prediction)
+            word_hash[word][SCORE].append(conf)
+    final_hash = {}
+    for key, item in word_hash.items():
+        mean_conf = sum(item[SCORE])/len(item[SCORE])
+        nr_of_majority = item[PREDICTION].count(majority_class)
+        half_length = len(item[PREDICTION])/2
+        if nr_of_majority > half_length:
+            most_common_predicted = inv_labelled_dict[majority_class]
+        else:
+            most_common_predicted = MINORITY_CLASSES
+        final_hash[key] = {PREDICTION: most_common_predicted, SCORE: mean_conf}
+
+    for key, item in final_hash.items():
+        print(key, item)
+
+
 
 
